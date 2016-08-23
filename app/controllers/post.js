4ac97eff5edc7f6be778e562a43b0e751725375c
Daniel Park <menoelpis@gmail.com>
2016-08-24 00:06:56 +0900

    Added Button and MouseEnter Event

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    sayHello: function() {
+      alert('Hello');
+    }
+  },
   title: 'My Blog Post',
   body: 'This is the body of my blog post',
   authors: ['William', 'Robert', 'Jason', 'Eunice'],
